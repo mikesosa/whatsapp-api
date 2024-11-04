94c1033b239ebb31f2113851c7bd3d10a210c1bb
mikesosa <michael@buydepa.com>
2024-11-04 13:33:39 -0500

    Add setup script for Docker and Nginx configuration

---
+++ .setup.sh
@@ -0,0 +1,28 @@
+sudo yum update -y
+sudo yum install git -y
+git clone https://github.com/mikesosa/whatsapp-api.git
+
+
+# Install Docker
+sudo yum install docker -y 
+
+# Start the Docker service
+sudo service docker start 
+
+# Add the ec2-user to the Docker group
+sudo usermod -aG docker ec2-user
+
+# Enable Docker to start on boot
+sudo systemctl enable docker 
+
+# Check Docker version
+docker --version
+
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+# Install certbot
+ sudo yum install -y certbot
+#  sudo certbot certonly --standalone -d notifications.stampo.app
